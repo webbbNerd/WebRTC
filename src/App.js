b1d07a52d0e270c054d28da9a28afde261ac28b2
webbbNerd <utkarshprofac@gmail.com>
2023-05-25 15:58:33 +0530

    adding login signup

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import Cookies from "js-cookie";
 import "./App.css";
 import LoginPage from "./components/Auth";
 import HomePage from "./components/Home";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const token = Cookies.get("jwttokenforwebrtc");
+  if (token) {
+    setIsLoggedIn(true);
+  }
 
   // Wrapper component for protected routes
   function ProtectedRoute({ path, element }) {
@@ -23,6 +28,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route
+            exact
             path="/login"
             element={<LoginPage setIsLoggedIn={setIsLoggedIn} />}
           />
